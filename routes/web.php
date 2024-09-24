7e278d7c62f92f39ca95744b657cef54e1ba9ebb
apurayy <apurayllpi@gmail.com>
2024-09-24 22:16:01 +0600

    frontend-start

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,16 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Front-end-part=======================
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
+
+//back-end-part==========================
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
